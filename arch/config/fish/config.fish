a1e2db259969191dc71802068c9eb5c371310e68
Noah Sakko <noahsakko@gmail.com>
2026-01-17 19:01:19 +0100

    Reorganised

---
+++ arch/config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+starship init fish | source
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+end
