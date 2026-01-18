2364535bd6fd0e437d57cdcfe5fb42f1caa68fbf
Noah Sakko <noahsakko@gmail.com>
2026-01-18 21:55:29 +0100

    Disable fish greeting message

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
   # Commands to run in interactive sessions can go here
   starship init fish | source
 
+  # Remove greeting message
+  function fish_greeting
+  end
+  
+  # Yazi alias
   function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     command yazi $argv --cwd-file="$tmp"
@@ -11,7 +16,7 @@ if status is-interactive
     rm -f -- "$tmp"
   end
 
+  # Set neovim as default editor 
   set -gx EDITOR nvim
   set -gx VISUAL nvim
-
 end
