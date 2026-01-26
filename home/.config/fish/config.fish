a0e538bfd0b96b6be44c2818534c88f807d3eb49
Noah Sakko <noahsakko@gmail.com>
2026-01-26 15:14:52 +0100

    Setup Github SSH keys

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,22 +1,31 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  starship init fish | source
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
 
-  # Remove greeting message
-  function fish_greeting
-  end
-  
-  # Yazi alias
-  function y
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    command yazi $argv --cwd-file="$tmp"
-    if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
-      builtin cd -- "$cwd"
+    # Remove greeting message
+    function fish_greeting
     end
-    rm -f -- "$tmp"
-  end
 
-  # Set neovim as default editor 
-  set -gx EDITOR nvim
-  set -gx VISUAL nvim
+    # Start SSH agent
+    eval (ssh-agent -c) >/dev/null 2>&1
+    ssh-add ~/.ssh/personal-github-key >/dev/null 2>&1
+    ssh-add ~/.ssh/school-gitlab-key >/dev/null 2>&1
+
+    # Yazi alias
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        command yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+    # Aliases
+    alias su "su -m"
+    alias cat bat
+
+    # Set neovim as default editor 
+    set -gx EDITOR nvim
+    set -gx VISUAL nvim
 end
