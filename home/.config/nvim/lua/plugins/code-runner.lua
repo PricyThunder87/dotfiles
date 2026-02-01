78ccfb63fa51057251318582290c8be7427bd9fa
Noah Sakko <noahsakko@gmail.com>
2026-02-02 00:41:03 +0100

    Add code-runner, nvim-surround, vim-repeat and telescope config files

---
+++ home/.config/nvim/lua/plugins/code-runner.lua
@@ -0,0 +1,13 @@
+return {
+  "CRAG666/code_runner.nvim",
+  config = true,
+  filetype = {
+    java = {
+      "cd $dir &&",
+      "javac $fileName &&",
+      "java $fileNameWithoutExt",
+    },
+    python = "python3 -u",
+    typescript = "deno run",
+  },
+}
