5f8d99cb6df1bedd685d73496de84c2e3c1c05e1
Noah Sakko <noahsakko@gmail.com>
2026-02-06 15:20:36 +0100

    add rust config to code-runner

--- stow-files/nvim/.config/nvim/lua/plugins/code-runner.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/code-runner.lua
@@ -1,13 +1,17 @@
 return {
-  "CRAG666/code_runner.nvim",
-  config = true,
-  filetype = {
-    java = {
-      "cd $dir &&",
-      "javac $fileName &&",
-      "java $fileNameWithoutExt",
+    "CRAG666/code_runner.nvim",
+    config = true,
+    filetype = {
+        java = {
+            "cd $dir &&",
+            "javac $fileName &&",
+            "java $fileNameWithoutExt",
+        },
+        python = "python3 -u",
+        typescript = "deno run",
+        rust = {
+            "cargo build",
+            "cargo run",
+        },
     },
-    python = "python3 -u",
-    typescript = "deno run",
-  },
 }
