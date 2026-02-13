d7311789ddb7834eed12e11f27a3772a9d71fed3
Noah Sakko <noahsakko@gmail.com>
2026-02-13 16:53:52 +0100

    Formatting change

--- stow-files/nvim/.config/nvim/lua/config/options.lua
+++ stow-files/nvim/.config/nvim/lua/config/options.lua
@@ -27,10 +27,10 @@ vim.diagnostic.config({
 
 -- Highlight yanked text
 vim.api.nvim_create_autocmd("TextYankPost", {
-  pattern = "*",
-  callback = function()
+    pattern = "*",
+    callback = function()
 	vim.highlight.on_yank({ higroup = "IncSearch", timeout = 200 })
-  end,
+    end,
 })
 
 -- Disable animations
