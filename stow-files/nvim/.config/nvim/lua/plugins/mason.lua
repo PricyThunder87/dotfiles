7b5b56928fa6c64cc9581db75d0c6a3e7c0d56a4
Noah Sakko <noahsakko>
2026-02-17 00:18:24 +0100

    Add Roslyn LSP config

--- stow-files/nvim/.config/nvim/lua/plugins/mason.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/mason.lua
@@ -8,4 +8,34 @@ return {
 	},
 	"neovim/nvim-lspconfig",
     },
+    config = function()
+	require("mason").setup({
+	    registries = {
+		"github:mason-org/mason-registry",
+		"github:Crashdummyy/mason-registry",
+	    }
+	})
+
+	vim.lsp.config("lua_ls", {
+	    settings = {
+		Lua = {
+		    diagnostics = {
+			globals = { "vim", "opts" },
+		    },
+		    runtime = {
+			version = "LuaJIT",
+		    },
+		    workspace = {
+			checkThirdParty = false,
+			library = vim.api.nvim_get_runtime_file("", true),
+		    },
+		    telemetry = {
+			enable = false,
+		    },
+		},
+	    },
+	})
+
+	vim.lsp.enable("lua_ls")
+    end,
 }
