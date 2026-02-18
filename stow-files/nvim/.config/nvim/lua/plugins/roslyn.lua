b02c9cdf461f390257baa50280f1e43ad27f148b
Noah Sakko <noahsakko>
2026-02-18 14:03:53 +0100

    Remove unnecessary comments

--- stow-files/nvim/.config/nvim/lua/plugins/roslyn.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/roslyn.lua
@@ -1,34 +1,48 @@
 return {
-  "seblyng/roslyn.nvim",
-  ft = { "cs", "razor" },
-  opts = {
-    -- Points to the Mason-installed binary
-    exe = vim.fn.stdpath("data") .. "/mason/bin/roslyn-language-server",
-    args = {
-      "--logLevel=Information",
-      "--extensionLogDirectory=" .. vim.fn.stdpath("cache") .. "/roslyn-logs",
+    "seblyng/roslyn.nvim",
+    ft = { "cs", "razor" },
+    opts = {
+	exe = vim.fn.stdpath("data") .. "/mason/bin/roslyn-language-server",
+	args = {
+	    "--logLevel=Information",
+	    "--extensionLogDirectory=", .. vim.fn.stdpath("cache") . "/roslyn-logs",
+	},
+	config = {
+	    on_attach = function(client, bufnr)
+		if vim.bo[bufnr].filetype == "razor" then
+		    client.server_capabilities.semanticTokensProvider = nil
+		end
+	    end,
+	    settings = {
+		["csharp"] = {
+		    background_analysis = {
+			dotnet_analyzer_diagnostics_scope = "openFiles",
+			dotnet_compiler_diagnostics_scope = "openFiles",
+		    },
+		},
+		["razor"] = {
+		    enabled = true,
+		},
+	    },
+	},
     },
     config = {
-      -- This is where the magic happens for performance
-      on_attach = function(client, bufnr)
-        -- Razor files send a flood of tokens that freeze Neovim. 
-        -- Disabling this usually restores 60fps typing.
-        if vim.bo[bufnr].filetype == "razor" then
-          client.server_capabilities.semanticTokensProvider = nil
-        end
-      end,
-      settings = {
-        ["csharp"] = {
-          -- Reduces background indexing load
-          background_analysis = {
-            dotnet_analyzer_diagnostics_scope = "openFiles",
-            dotnet_compiler_diagnostics_scope = "openFiles",
-          },
-        },
-        ["razor"] = {
-          enabled = true,
-        },
-      },
+	on_attach = function(client, bufnr)
+	    if vim.bo[bufnr].filetype == "razor" then
+		client.server_capabilities.semanticTokensProvider = nil
+	    end
+	end,
+	settings = {
+	    ["csharp"] = {
+		background_analysis = {
+		    dotnet_analyzer_diagnostics_scope = "openFiles",
+		    dotnet_compiler_diagnostics_scope = "openFiles",
+		},
+	    },
+	    ["razor"] = {
+		enabled = true,
+	    },
+	},
     },
-  },
+},
 }
