76c8224bdb46e562279123861d1baa9e51439aa3
Noah Sakko <noahsakko@gmail.com>
2026-02-18 13:55:22 +0100

    Switch to different gruvbox repo

--- stow-files/nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,10 @@
 return {
-    "ellisonleao/gruvbox.nvim",
+    "https://gitlab.com/motaz-shokry/gruvbox.nvim",
     priority = 1000 ,
     config = function()
+	require("gruvbox").setup({
+	    variant = "medium",
+	})
 	vim.cmd.colorscheme "gruvbox"
     end,
     opts = ...
