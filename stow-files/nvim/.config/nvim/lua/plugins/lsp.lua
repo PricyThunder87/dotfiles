06c2ba0f1175c394f5768ed77e1825da679cd253
Noah Sakko <noahsakko>
2026-02-18 23:18:09 +0100

    Reconfigure LSP config to comply with new standard

---
+++ stow-files/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,33 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+	require("mason").setup({
+	    registries = {
+		"github:mason-org/mason-registry",
+		"github:Crashdummyy/mason-registry",
+	    }
+	})
+	require("mason-lspconfig").setup({
+	    ensure_installed = {
+		"lua_ls",
+	    },
+	})
+
+	vim.lsp.config("lua_ls", {
+	    settings = {
+		Lua = {
+		    diagnostics = {
+			globals = { "vim" },
+		    },
+		},
+	    },
+	})
+
+	vim.lsp.enable("lua_ls")
+	vim.lsp.enable("roslyn")
+    end,
+}
