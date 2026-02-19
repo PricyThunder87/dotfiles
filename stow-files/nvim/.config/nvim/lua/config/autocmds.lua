c2437b5ee47437b3ab6b666e4ce1f26e32e8f375
Noah Sakko <noahsakko>
2026-02-19 21:50:48 +0100

    Remove trailing whitespaces when text is inserted

--- stow-files/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow-files/nvim/.config/nvim/lua/config/autocmds.lua
@@ -8,14 +8,27 @@
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 vim.api.nvim_create_autocmd("InsertEnter", {
-  callback = function()
-    vim.opt.relativenumber = false
-  end,
+    callback = function()
+	vim.opt.relativenumber = false
+    end,
 })
 
+--[[
+Deletes all trailing whitespaces in a file if it's not binary nor a diff.
+]]--
+function _G.trim_trailing_whitespaces()
+    if not vim.o.binary and vim.o.filetype ~= 'diff' then
+        local current_view = vim.fn.winsaveview()
+        vim.cmd([[keeppatterns %s/\s\+$//e]])
+        vim.fn.winrestview(current_view)
+    end
+end
+
+-- Show relative line numbers in normal mode
 vim.api.nvim_create_autocmd("InsertLeave", {
   callback = function()
     vim.opt.relativenumber = true
+    trim_trailing_whitespaces()
   end,
 })
 
