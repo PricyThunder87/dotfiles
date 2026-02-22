8902ca8d208cbfd44d7565fb5dc64cd02a0a41a0
Noah Sakko <noahsakko@gmail.com>
2026-02-22 11:57:16 +0100

    Remove unnecessary comments

--- stow-files/nvim/.config/nvim/lua/config/autocmds.lua
+++ stow-files/nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,21 +1,10 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
-
 vim.api.nvim_create_autocmd("InsertEnter", {
     callback = function()
 	vim.opt.relativenumber = false
     end,
 })
 
---[[
-Deletes all trailing whitespaces in a file if it's not binary nor a diff.
-]]--
+-- Delete all trailing whitespaces in a file if it's not binary nor a diff
 function _G.trim_trailing_whitespaces()
     if not vim.o.binary and vim.o.filetype ~= 'diff' then
         local current_view = vim.fn.winsaveview()
