ccff9dc3103e61c7b93445d091789c09eba36102
Noah Sakko <noahsakko>
2026-02-24 17:31:16 +0100

    Add command feedback to lualine

--- stow-files/nvim/.config/nvim/lua/plugins/lualine.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,4 +1,18 @@
 return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
+    opts = {
+        sections = {
+            lualine_x = {
+                {
+                    function()
+                        return require("noice").api.status.command.get()
+                    end,
+                    cond = function()
+                        return package.loaded["noice"] and require("noice").api.status.command.has()
+                    end,
+                },
+            },
+        },
+    },
 }
