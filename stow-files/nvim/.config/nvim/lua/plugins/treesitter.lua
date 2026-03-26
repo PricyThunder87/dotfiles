0cd0d0bed52389c86c7393368f7e7d6622287c2b
Noah Sakko <noahsakko@gmail.com>
2026-03-26 09:52:23 +0100

    Add .razor file support

--- stow-files/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,25 @@
 return {
     'nvim-treesitter/nvim-treesitter',
     lazy = false,
-    build = ':TSUpdate'
+    build = ':TSUpdate',
+    dependencies = {},
+    opts = {
+	ensure_installed = { "c_sharp", "html" },
+	highlight = { enable = true },
+    },
+    init = function()
+	vim.filetype.add({
+	    extension = {
+		razor = "razor",
+		cshtml = "razor",
+	    }
+	})
+	vim.treesitter.language.register('razor', 'razor')
+	vim.api.nvim_create_autocmd("FileType", {
+	    pattern = "razor",
+	    callback = function()
+		vim.treesitter.start()
+	    end
+	})
+    end
 }
