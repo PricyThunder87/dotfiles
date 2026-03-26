d6223abd62610acb490f5f58385f18b4282cabf1
Noah Sakko <noahsakko@gmail.com>
2026-03-26 10:43:50 +0100

    Add keybinds to close buffer

--- stow-files/nvim/.config/nvim/lua/plugins/bufferline.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -4,5 +4,7 @@ return {
     dependencies = 'nvim-tree/nvim-web-devicons',
     config = function()
 	require("bufferline").setup()
+	vim.keymap.set("n", "<leader>bd", "<cmd>bd<cr>")
+	vim.keymap.set("n", "<leader>bo", "<cmd>BufferLineCloseOthers<cr>")
     end
 }
