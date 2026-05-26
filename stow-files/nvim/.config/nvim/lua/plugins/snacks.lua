7b4993cb9466f9d1f3b94772c90b5e94fe2eac2b
Noah Sakko <noahsakko@gmail.com>
2026-05-26 22:38:23 +0200

    Add easy dotnet new file integration

--- stow-files/nvim/.config/nvim/lua/plugins/snacks.lua
+++ stow-files/nvim/.config/nvim/lua/plugins/snacks.lua
@@ -33,7 +33,36 @@ return {
 	picker =
 	{
 	    enabled = true,
-	    sources = { explorer = { win = { list = { wo = { number = true, relativenumber = true } } } } }
+	    sources = {
+		explorer = {
+		    win = {
+			list = {
+			    wo = {
+				number = true,
+				relativenumber = true
+			    },
+			    keys = {
+				["A"] = "explorer_add_dotnet",
+			    }
+			}
+		    },
+		    actions = {
+			explorer_add_dotnet = function(picker)
+			    local dir = picker:dir()
+			    local easydotnet = require("easy-dotnet")
+
+			    easydotnet.create_new_item(dir, function(item_path)
+				local tree = require("snacks.explorer.tree")
+				local actions = require("snacks.explorer.actions")
+				tree:open(dir)
+				tree:refresh(dir)
+				actions.update(picker, { target = item_path })
+				picker:focus()
+			    end)
+			end,
+		    }
+		}
+	    }
 	},
 	notifier = { enabled = false },
 	quickfile = { enabled = true },
