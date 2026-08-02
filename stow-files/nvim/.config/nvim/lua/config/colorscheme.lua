494437284e7e6e55f852271063d1ae8c4b7dc10e
Noah Sakko <noahsakko@gmail.com>
2026-08-02 14:16:35 +0200

    Switch to Cendre color scheme

--- stow-files/nvim/.config/nvim/lua/config/colorscheme.lua
+++ stow-files/nvim/.config/nvim/lua/config/colorscheme.lua
@@ -1,11 +1,19 @@
 vim.cmd.colorscheme "retrobox"
 
-vim.pack.add { { src = "https://gitlab.com/motaz-shokry/gruvbox.nvim", name = "gruvbox" } }
-require("gruvbox").setup {
-  variant = "medium",
-  enable = {
-    lualine = false,
-  },
+vim.pack.add {
+  { src = "https://github.com/Aejkatappaja/cendre", name = "cendre" },
+  -- { src = "https://gitlab.com/motaz-shokry/gruvbox.nvim", name = "gruvbox" },
 }
 
-vim.cmd.colorscheme "gruvbox"
+require("cendre").setup {
+  background = "soft",
+}
+
+-- require("gruvbox").setup {
+--   variant = "medium",
+--   enable = {
+--     lualine = false,
+--   },
+-- }
+
+vim.cmd.colorscheme "cendre"
