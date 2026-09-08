52efee27cc33c48700e15cd69d792ae0b5a17eff
Noah Sakko <noahsakko@gmail.com>
2026-09-08 16:24:42 +0200

    Add alias for paru --noconfirm

--- stow-files/fish/.config/fish/config.fish
+++ stow-files/fish/.config/fish/config.fish
@@ -161,6 +161,7 @@ if status is-interactive
 
     # Aliases
     alias gittree "git ls-files | tree --fromfile -a"
+    alias paru "paru --noconfirm"
     alias cp "cp -v"
     alias oc "opencode ./"
     alias nv "nvim"
